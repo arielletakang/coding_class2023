f9065421e6431d06a5e2095494fbf01e86a89479
arielletakang <arielleashu@gmail.com>
2023-09-24 12:28:46 -0400

    What i did in class

---
+++ Tello-master/1st assignment.py
@@ -0,0 +1,35 @@
+# TRY THE FOLLOWING
+# ASSIGN THESE NUMBERS TO VARIABLES
+# TRY THREE TYPES F MATH WITH SAID VARIABLES: ADDITION, SUBTRACTION, DIVISION
+# ASSIGN NEW NUMBERS TO SAID VARIABLES ONCE UR DONE
+# NAME EVERYTHING DIFF THAN MAURA DID
+# ADD, SUBTRACT, AND MULTIPLY IN LISTS
+
+# VARIABLES I USED
+a = 2
+b = 6
+c = 8
+
+# THE RESULT OF A BEING USED
+aResult = (a+a) * (a+b)
+#PRINTING A'S VALUE
+print(aResult)
+bResult = (b+b) * (b+c)
+# PRINTING B'S VALUE
+print(bResult)
+cResult = (c+c) * (c+b)
+print(cResult)
+first_example = (aResult + bResult + cResult)
+print(first_example)
+
+
+# INDEX & LIST EXAMPLE
+robin = (7,11,3)
+A = robin[0]
+B = robin[1]
+Answer = (A + B)
+print(Answer)
+
+#TRYING CONDITIONS
+#Make a conditional statement that keeps adding a variable to "Answer"
+#Until it reaches a certian number 
